80ee460590cb1b2006f6509143c3a67c5176def6
dizon108 <dizon108@mail.chapman.edu>
2016-09-16 11:47:29 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameOfLife
+//
+//  Created by Reese Dizon on 9/16/16.
+//  Copyright © 2016 Chapman University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
